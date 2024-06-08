2b12bfc7ba79b8ff1f3ed31ce098251933b7b77c
mateuslph <mlptche@gmail.com>
2024-06-08 19:14:42 -0300

    adiciona react-grid-system e cria sua estilizacao para card

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-grid-system";
 import { CampoTexto } from "./componentes/CampoTexto/CampoTexto";
 import { Card } from "./componentes/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
@@ -8,15 +9,21 @@ function App() {
   return (
     <ProvedorTema>
       <Estilos />
-      <Card>
-        <Tipografia variante="h1" componente="h1">
-          Freelando
-        </Tipografia>
-        <Tipografia variante="body" componente="body">
-          Crie seu perfil gratuitamente para começar a trabalhar com seus melhores freelacers
-        </Tipografia>
-        <CampoTexto titulo="Nome Completo" />
-      </Card>
+      <Container>
+        <Row justify="center">
+          <Col lg={6}>
+            <Card>
+              <Tipografia variante="h1" componente="h1">
+                Freelando
+              </Tipografia>
+              <Tipografia variante="body" componente="body">
+                Crie seu perfil gratuitamente para começar a trabalhar com seus melhores freelacers
+              </Tipografia>
+              <CampoTexto titulo="Nome Completo" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </ProvedorTema>
   );
 }
